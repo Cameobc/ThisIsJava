5e68819b1b3403832023dd172f15cdb92dbe95ba
Cameobc <dkqtk@naver.com>
2022-04-24 10:15:55 +0900

    220411_이것이 자바다 7.7.2

--- src/main/java/inheritance/ChildExample.java
+++ src/main/java/inheritance/ChildExample.java
@@ -7,6 +7,7 @@ public class ChildExample {
 
         Parent parent = child;
         parent.method1();
-        parent.method2();
+        parent.method2(); // 오버라이드 된 메서드가 호출된다.
+        // 자식에게만 정의된 메서드는 부모가 호출할 수 없다.
     }
 }
