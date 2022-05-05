7ff1d266ff378ce9e563a43572f41adfcc19a43c
Cameobc <dkqtk@naver.com>
2022-05-06 00:19:27 +0900

    220504_이것이 자바다 14-5-5

---
+++ src/main/java/lamda/api/opertator/OpertaorExample.java
@@ -0,0 +1,42 @@
+package lamda.api.opertator;
+
+import java.util.function.IntBinaryOperator;
+
+public class OpertaorExample {
+
+    private static int[] scores = {92, 95, 87};
+
+    public static int maxOrMin(IntBinaryOperator operator) {
+        int result = scores[0];
+        for (int score : scores) {
+            result = operator.applyAsInt(result, score);
+        }
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        // 최대값
+        int max = maxOrMin(
+                (a,b) -> {
+                    if (a >= b) {
+                        return a;
+                    }else{
+                        return b;
+                    }
+                }
+        );
+        System.out.println("최대값 : " + max);
+
+        // 최소값
+        int min = maxOrMin(
+                (a,b) -> {
+                    if (a <= b) {
+                        return a;
+                    }else{
+                        return b;
+                    }
+                }
+        );
+        System.out.println("최소값 : " + min);
+    }
+}
